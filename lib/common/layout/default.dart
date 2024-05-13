2a1661af42346145200b78a480d96ea14040326d
academy3746 <academy3746@gmail.com>
2024-05-13 13:33:08 +0900

    :sparkles: Customizing Scaffold Widget

--- lib/common/layout/default.dart
+++ lib/common/layout/default.dart
@@ -1,21 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:login/common/constant/sizes.dart';
 
 class DefaultScaffold extends StatelessWidget {
   const DefaultScaffold({
     super.key,
     required this.child,
     this.backgroundColor,
+    this.title,
+    this.centerTitle,
+    this.bottomNavigationBar,
   });
 
   final Widget child;
 
   final Color? backgroundColor;
 
+  final String? title;
+
+  final bool? centerTitle;
+
+  final Widget? bottomNavigationBar;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundColor ?? Colors.white,
+      appBar: _buildAppBar(),
       body: child,
+      bottomNavigationBar: bottomNavigationBar,
+    );
+  }
+
+  AppBar? _buildAppBar() {
+    if (title == null) {
+      return null;
+    }
+
+    return AppBar(
+      backgroundColor: Colors.white,
+      elevation: 0,
+      scrolledUnderElevation: 0,
+      centerTitle: centerTitle ?? false,
+      title: Text(
+        title!,
+        style: const TextStyle(
+          fontSize: Sizes.size18,
+          fontWeight: FontWeight.w700,
+        ),
+      ),
+      foregroundColor: Colors.black,
     );
   }
 }
--- lib/common/views/root_tab.dart
+++ lib/common/views/root_tab.dart
@@ -1,15 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:login/common/constant/colors.dart';
+import 'package:login/common/constant/sizes.dart';
 import 'package:login/common/layout/default.dart';
 
-class RootTab extends StatelessWidget {
+class RootTab extends StatefulWidget {
   const RootTab({super.key});
 
   @override
+  State<RootTab> createState() => _RootTabState();
+}
+
+class _RootTabState extends State<RootTab> {
+  int currentIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return const DefaultScaffold(
-      child: Center(
+    return DefaultScaffold(
+      title: 'Yamette!',
+      centerTitle: true,
+      bottomNavigationBar: _buildBottomWidget(),
+      child: const Center(
         child: Text('Root Tab'),
       ),
+    );
+  }
+
+  Widget _buildBottomWidget() {
+    return BottomNavigationBar(
+      backgroundColor: Colors.black87,
+      selectedItemColor: primaryColor,
+      unselectedItemColor: bodyTextColor,
+      selectedFontSize: Sizes.size10,
+      unselectedFontSize: Sizes.size10,
+      type: BottomNavigationBarType.fixed,
+      currentIndex: currentIndex,
+      onTap: (index) {
+        setState(() {
+          currentIndex = index;
+        });
+      },
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home_outlined),
+          label: 'HOME',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.fastfood_outlined),
+          label: 'FOOD',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.receipt_long_outlined),
+          label: 'ORDER',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person_outline_outlined),
+          label: 'PROFILE',
+        ),
+      ],
     );
   }
 }

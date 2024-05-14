46fb650a5d9d6d81a22794e63b6a3385ddf4c865
academy3746 <academy3746@gmail.com>
2024-05-14 20:34:45 +0900

    :memo: dio.dart 주석 라인 보강

--- lib/common/dio/dio.dart
+++ lib/common/dio/dio.dart
@@ -14,19 +14,27 @@ class CarrierHasArrived extends Interceptor {
     RequestInterceptorHandler handler,
   ) async {
     if (options.headers['accessToken'] == 'true') {
+      /// Implicit Headers 삭제
       options.headers.remove('accessToken');
 
       final token = await storage.read(key: accessTokenKey);
 
-      options.headers.addAll({'Authorization': 'Bearer $token'});
+      /// 유효한 토큰 값으로 대체
+      options.headers.addAll({
+        'Authorization': 'Bearer $token',
+      });
     }
 
     if (options.headers['refreshToken'] == 'true') {
+      /// Implicit Headers 삭제
       options.headers.remove('refreshToken');
 
       final token = await storage.read(key: refreshTokenKey);
 
-      options.headers.addAll({'Authorization': 'Bearer $token'});
+      /// 유효한 토큰 값으로 대체
+      options.headers.addAll({
+        'Authorization': 'Bearer $token',
+      });
     }
 
     return super.onRequest(options, handler);
@@ -44,22 +52,29 @@ class CarrierHasArrived extends Interceptor {
     DioException err,
     ErrorInterceptorHandler handler,
   ) async {
-    print('[ERR] [${err.requestOptions.method}] [${err.requestOptions.uri}]');
-
+    /// [401] accessToken Expiration
+    /// 1. 토큰 재발급 요청 [auth → token]
+    /// 2. 새로운 토큰 재요청 [refreshToken → accessToken]
     final refreshToken = await storage.read(key: refreshTokenKey);
 
     if (refreshToken == null) {
+      /// 1. refreshToken Expiration
+      /// 2. onGenerate Error
+      /// 3. Exit Process
       return handler.reject(err);
     }
 
+    /// [Bool] 토큰 만료 Status Code
     final isStatus401 = err.response?.statusCode == 401;
 
+    /// [Bool] 올바른 경로로 토큰이 요청 되었는지 여부 체크
     final isPathRefresh = err.requestOptions.path == '/auth/token';
 
     if (isStatus401 && !isPathRefresh) {
       final dio = Dio();
 
       try {
+        /// Request (Refresh) New Token
         final res = await dio.post(
           'http://$ip/auth/token',
           options: Options(
@@ -69,23 +84,30 @@ class CarrierHasArrived extends Interceptor {
           ),
         );
 
+        /// Actual Token Value
         final accessToken = res.data['accessToken'];
 
         final options = err.requestOptions;
 
+        /// Receive New accessToken
         options.headers.addAll({
           'Authorization': 'Bearer $accessToken',
         });
 
+        /// Overwrite accessToken Value
         await storage.write(
           key: accessTokenKey,
           value: accessToken,
         );
 
+        /// New Request
         final response = await dio.fetch(options);
 
+        /// [200] Exit Process without Error
         return handler.resolve(response);
       } on DioException catch (e) {
+        /// 1. Could not refresh token
+        /// 2. Throw Dio Error
         return handler.reject(e);
       }
     }

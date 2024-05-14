f48418e39ad4b163a09bba955c88b1d7ae01628c
academy3746 <academy3746@gmail.com>
2024-05-14 13:39:52 +0900

    :art: Detail Screen Scrollable

--- lib/features/store/views/detail_screen.dart
+++ lib/features/store/views/detail_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:login/common/constant/gaps.dart';
 import 'package:login/common/constant/sizes.dart';
 import 'package:login/common/layout/default.dart';
 import 'package:login/features/product/widgets/product_card.dart';
@@ -24,28 +23,66 @@ class _StoreDetailScreenState extends State<StoreDetailScreen> {
         margin: const EdgeInsets.symmetric(
           vertical: Sizes.size16,
         ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            StoreCard(
-              image: Image.asset('assets/images/food/pizza_ddeok_bok_gi.jpg'),
-              name: '피자타임',
-              tags: const ['불고기', '페페로니', '할라피뇨'],
-              ratingsCount: 100,
-              deliveryTime: 20,
-              deliveryFee: 2500,
-              ratings: 4.5,
-              isDetail: true,
-              detail: '할라피뇨 불고기 피자 존맛탱...',
-            ),
-            Gaps.v24,
-            Container(
-              margin: const EdgeInsets.symmetric(
-                horizontal: Sizes.size20,
-              ),
-              child: const ProductCard(),
-            ),
+        child: CustomScrollView(
+          slivers: [
+            _buildTop(),
+            _buildLabel(),
+            _buildProduct(),
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildLabel() {
+    return SliverToBoxAdapter(
+      child: Container(
+        margin: const EdgeInsets.only(
+          top: Sizes.size24,
+          left: Sizes.size20,
+          right: Sizes.size20,
+        ),
+        child: const Text(
+          '메뉴',
+          style: TextStyle(
+            fontSize: Sizes.size18,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTop() {
+    return SliverToBoxAdapter(
+      child: StoreCard(
+        image: Image.asset('assets/images/food/pizza_ddeok_bok_gi.jpg'),
+        name: '피자타임',
+        tags: const ['불고기', '페페로니', '할라피뇨'],
+        ratingsCount: 100,
+        deliveryTime: 20,
+        deliveryFee: 2500,
+        ratings: 4.5,
+        isDetail: true,
+        detail: '할라피뇨 불고기 피자 존맛탱...',
+      ),
+    );
+  }
+
+  Widget _buildProduct() {
+    return SliverPadding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: Sizes.size20,
+      ),
+      sliver: SliverList(
+        delegate: SliverChildBuilderDelegate(
+          (context, index) => Container(
+            margin: const EdgeInsets.only(
+              top: Sizes.size16,
+            ),
+            child: const ProductCard(),
+          ),
+          childCount: 10,
         ),
       ),
     );
